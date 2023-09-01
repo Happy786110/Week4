c64dc9337887fb62adf30e8ac79fd0dbe7a26078
Happy786110 <143800959+Happy786110@users.noreply.github.com>
2023-09-01 22:24:49 +1000

    80 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -7,10 +7,17 @@ public class LoadAssets : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject redObj;
+       
+    [SerializeField] 
+    private GameObject blueObj;
 
     void Start()
     {
-        
+
+        Application.targetFrameRate = 60;
+        GameObject redInstance = Instantiate(redObj, new Vector3(2f,0f,0f), Quaternion.identity);
+
+        GameObject blueInstance = Instantiate(blueObj, new Vector3(-2f,0f,0f), Quaternion.identity);
     }
 
     // Update is called once per frame
